d14b79ec723c726dc5e4e24ef2bd2ec8d1399edc
developer <emakuthi@gmail.com>
2019-08-02 23:14:16 +0300

    add methods

--- src/main/java/com/example/demo/BookingController.java
+++ src/main/java/com/example/demo/BookingController.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value ="/bookings")
@@ -22,6 +24,12 @@ public class BookingController {
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public List<HotelBooking> getAll(){
         return bookings;
+    }
+    @RequestMapping(value = "/affordable/{price}", method = RequestMethod.GET)
+    public List<HotelBooking> getAffordable(@PathVariable double price){
+        return  bookings.stream().filter(x -> x.getPricePerNight()<= price)
+                .collect(Collectors.toList());
+
     }
 
 }
