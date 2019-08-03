a0afd830f0c0d194ec96771db9938425df2a66e7
developer <emakuthi@gmail.com>
2019-08-03 20:30:57 +0300

    change the data to be stored on database

--- src/main/java/com/example/demo/HotelBooking.java
+++ src/main/java/com/example/demo/HotelBooking.java
@@ -1,39 +1,37 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
 public class HotelBooking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
     private String hotelName;
     private Double pricePerNight;
     private int nbOfNights;
 
+    public HotelBooking() {}
     public HotelBooking(String hotelName, Double pricePerNight, int nbOfNights) {
         this.hotelName = hotelName;
         this.pricePerNight = pricePerNight;
         this.nbOfNights = nbOfNights;
     }
-
     public String getHotelName() {
         return hotelName;
-    }
-
-    public void setHotelName(String hotelName) {
-        this.hotelName = hotelName;
     }
 
     public Double getPricePerNight() {
         return pricePerNight;
     }
-
-    public void setPricePerNight(Double pricePerNight) {
-        this.pricePerNight = pricePerNight;
-    }
-
     public int getNbOfNights() {
         return nbOfNights;
     }
-    public void setNbOfNights(int nbOfNights) {
-        this.nbOfNights = nbOfNights;
-    }
-
     public Double getTotalPrice(){
         return pricePerNight*nbOfNights;
     }
